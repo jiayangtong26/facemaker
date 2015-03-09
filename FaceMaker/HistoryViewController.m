70f1131a3858384d341f681cf0ef5dd33937a4bf
Jiayang Tong <jiayangtong@uchicago.edu>
2015-03-08 23:44:16 -0500

    modify the viewdidload and viewwillappear

--- FaceMaker/HistoryViewController.m
+++ FaceMaker/HistoryViewController.m
@@ -18,6 +18,48 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor blackColor]];
+    //[self.view setAlpha:0.7];
+    self.scrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height * 3, self.view.frame.size.width, self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 3 - self.tabBarController.tabBar.frame.size.height * 2)];
+    [self.scrollview setPagingEnabled:YES];
+    [self.scrollview setScrollEnabled:YES];
+    [self.scrollview setBounces:YES];
+    [self.view addSubview:self.scrollview];
+    // Width constraint
+    /*[self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:nil
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0
+                                                           constant:self.view.frame.size.width]];
+    
+    // Height constraint
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:nil
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:1.0
+                                                           constant:self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 3 - self.tabBarController.tabBar.frame.size.height * 2]];
+    // Center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
+    // Center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:0.0]];*/
+    self.automaticallyAdjustsScrollViewInsets = NO;
 
 }
 
@@ -34,57 +76,11 @@
                 [self.assets addObject:result];
             	}
             else{
-                NSLog(@"%d", self.assets.count);
-                
-                self.scrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height * 1.5, self.view.frame.size.width, self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 1.5 - self.tabBarController.tabBar.frame.size.height)];
-                [self.scrollview setPagingEnabled:YES];
-                [self.scrollview setScrollEnabled:YES];
-                [self.scrollview setBounces:YES];
-                [self.view addSubview:self.scrollview];
-                // Width constraint
-                [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
-                                                                      attribute:NSLayoutAttributeWidth
-                                                                      relatedBy:NSLayoutRelationEqual
-                                                                         toItem:nil
-                                                                      attribute:NSLayoutAttributeWidth
-                                                                     multiplier:1.0
-                                                                       constant:self.view.frame.size.width]];
-                
-                // Height constraint
-                [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
-                                                                      attribute:NSLayoutAttributeHeight
-                                                                      relatedBy:NSLayoutRelationEqual
-                                                                         toItem:nil
-                                                                      attribute:NSLayoutAttributeHeight
-                                                                     multiplier:1.0
-                                                                       constant:self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 1.5 - self.tabBarController.tabBar.frame.size.height]];
-                // Center horizontally
-                [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
-                                                                      attribute:NSLayoutAttributeCenterX
-                                                                      relatedBy:NSLayoutRelationEqual
-                                                                         toItem:self.view
-                                                                      attribute:NSLayoutAttributeCenterX
-                                                                     multiplier:1.0
-                                                                       constant:0.0]];
-                
-                // Center vertically
-                [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollview
-                                                                      attribute:NSLayoutAttributeCenterY
-                                                                      relatedBy:NSLayoutRelationEqual
-                                                                         toItem:self.view
-                                                                      attribute:NSLayoutAttributeCenterY
-                                                                     multiplier:1.0
-                                                                       constant:0.0]];
-                self.scrollview.contentSize = CGSizeMake(self.assets.count *self.view.frame.size.width, self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 1.5 - self.tabBarController.tabBar.frame.size.height);
-                self.automaticallyAdjustsScrollViewInsets = NO;
-                NSLog(@"width: %f", self.view.frame.size.width);
-                NSLog(@"width: %f", self.view.frame.size.height);
-                NSLog(@"width - : %f", self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height - self.tabBarController.tabBar.frame.size.height);
-                NSLog(@"top y: %f", self.navigationController.navigationBar.frame.size.height);
+                self.scrollview.contentSize = CGSizeMake(self.assets.count *self.view.frame.size.width, self.view.frame.size.height - self.navigationController.navigationBar.frame.size.height * 3 - self.tabBarController.tabBar.frame.size.height * 2);
                 for(int i = 0; i < self.assets.count; i++){
                     ALAsset *asset = [self.assets objectAtIndex:i];
                     UIImage* image = [UIImage imageWithCGImage:[asset aspectRatioThumbnail]];
-                    UIImageView * imageview = [[UIImageView alloc]initWithFrame:CGRectMake(i*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height - self.navigationController.navigationBar.frame.size.height)];
+                    UIImageView * imageview = [[UIImageView alloc]initWithFrame:CGRectMake(i*self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height * 3 - self.navigationController.navigationBar.frame.size.height * 2)];
                     [imageview setImage:image];
                     [self.scrollview addSubview:imageview];
                     [self.scrollview bringSubviewToFront:imageview];
@@ -107,7 +103,6 @@
            					 failureBlock: ^(NSError *error) {
                						 NSLog(@"Failure");
                					 }];
-    
 }
 
 - (void)didReceiveMemoryWarning {
